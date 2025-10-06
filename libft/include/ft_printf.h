091d346926255ebd8ac763647a0acb98f4817e77
X03phy <elio.bonutto@gmail.com>
2025-10-06 09:47:15 +0200

    feat: add ft_dprintf

---
+++ libft/include/ft_printf.h
@@ -0,0 +1,6 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+int ft_dprintf( int fd, const char *format, ... );
+
+#endif
---
+++ libft/src/ft_printf/ft_dprintf.c
@@ -0,0 +1,54 @@
+#include "ft_printf.h"
+
+/* For va_list, va_start(), va_arg(), va_end() */
+#include <stdarg.h>
+
+/* For write() */
+#include <unistd.h>
+
+/* For ft_strlen() */
+#include "string.h"
+
+static int ft_convert( const int fd, const char spec, va_list ap )
+{
+	if ( spec == 's' )
+	{
+		char *str = va_arg( ap, char * );
+		return ( write( fd, str, ft_strlen( str ) ) );
+	}
+
+	else if ( spec == 'c' )
+	{
+		char c = va_arg( ap, int );
+		return ( write( fd, &c, 1 ) );
+	}
+
+	else
+		return ( write( fd, "%", 1 ) );
+}
+
+int ft_dprintf( int fd, const char *format, ... )
+{
+	int count = 0;
+	va_list ap;
+
+	if ( fd < 0 || 1023 < fd || format == NULL )
+		return ( -1 );
+
+	va_start( ap, format );
+	for ( int i = 0; format[i]; i += 1 )
+	{
+		if ( format[i] == '%' )
+		{
+			i += 1;
+			if ( !format[i] )
+				break;
+			count += ft_convert( fd, format[i], ap );
+		}
+		else
+			count += write( fd, &format[i], 1 );
+	}
+	va_end( ap );
+
+	return ( count );
+}
