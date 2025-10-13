649fcef535af86d803f21edc4024139fb54b04e6
X03phy <elio.bonutto@gmail.com>
2025-10-13 23:44:15 +0200

    fix: architecture

--- include/ft_nm.h
+++ include/ft_nm.h
@@ -6,7 +6,7 @@
 /*   By: x03phy <x03phy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 23:32:19 by x03phy            #+#    #+#             */
-/*   Updated: 2025/10/13 23:32:20 by x03phy           ###   ########.fr       */
+/*   Updated: 2025/10/13 23:43:19 by x03phy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ typedef struct s_opts
 int parse_input( t_opts *opts, int argc, char **argv );
 
 /* ft_nm */
+int process_elf32( void *map );
 int process_elf64( void *map );
 int process_file( const char *filename, int fd, struct stat *st, void *map );
 int ft_nm_wrapper( t_opts *opts );
