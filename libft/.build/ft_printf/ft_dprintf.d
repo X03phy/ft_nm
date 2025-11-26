ca849ba926630baece1605a4cbca0db91b5eee5b
X03phy <elio.bonutto@gmail.com>
2025-11-26 01:42:26 +0100

    feat: add some tests

--- libft/.build/ft_printf/ft_dprintf.d
+++
@@ -1,2 +0,0 @@
-.build/ft_printf/ft_dprintf.o: src/ft_printf/ft_dprintf.c \
- include/ft_printf.h include/string.h
--- libft/.build/linked_list/ft_lstget.d
+++
@@ -1,2 +0,0 @@
-.build/linked_list/ft_lstget.o: src/linked_list/ft_lstget.c \
- include/linked_list.h
